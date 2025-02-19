70ef3dfbb431309f42e3f4b4ec9887a0af55bb20
z4sythe <zforsythe@student.umgc.edu>
2025-02-19 17:13:24 -0500

    Updated .env path

--- team_b/yappy/lib/env.dart
+++ team_b/yappy/lib/env.dart
@@ -3,7 +3,7 @@ import 'package:envied/envied.dart';
 part 'env.g.dart';
 
 // Run `flutter pub run build_runner build` to generate the env.g.dart file
-@Envied(path: 'assets/.env')
+@Envied(path: '.env')
 abstract class Env {
     @EnviedField(varName: 'OPENAI_API_KEY')
     static const String apiKey = _Env.apiKey;
