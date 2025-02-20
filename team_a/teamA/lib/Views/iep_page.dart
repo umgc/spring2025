7e43ea58ad6f9fabfad84d56ddc1f900a9b61b3f
Hammes23 <andy.hammes23@gmail.com>
2025-02-20 17:18:31 -0500

    #51 UI Elements added to IEP page

--- team_a/teamA/lib/Views/iep_page.dart
+++ team_a/teamA/lib/Views/iep_page.dart
@@ -12,19 +12,110 @@ class IepPage extends StatefulWidget{
 }
 
 class _IepPageState extends State{
-
+  bool? isChecked1 = false;
+  bool? isChecked2 = false;
+  bool? isChecked3 = false;
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: CustomAppBar(title: 'Individual Education Plans', userprofileurl: MoodleApiSingleton().moodleProfileImage ?? ''),
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children:[
-            // Add Content here
             Text(
-              'Place content here',
-              style: TextStyle(fontSize: 20),
+              'Individual Education Plan Page',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Enroll Student in New IEP',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 500,
+              padding: EdgeInsets.only(top: 20, bottom: 20),
+              child: TextField(
+                        decoration: InputDecoration(
+                        labelText: 'Enter First Name', // Label inside the text box
+                        border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(
+              width: 500,
+              padding: EdgeInsets.only(bottom: 20),
+              child: TextField(
+                        decoration: InputDecoration(
+                        labelText: 'Enter Last Name', // Label inside the text box
+                        border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            DropdownMenu(
+              helperText: 'Course',
+              width: 500,
+              dropdownMenuEntries: <DropdownMenuEntry<Color>>[
+                DropdownMenuEntry(value: Colors.red, label: 'red'),
+                DropdownMenuEntry(value: Colors.blue, label: 'blue')
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked1,
+                  onChanged: (newBool) {
+                    setState(() {
+                      isChecked1 = newBool;
+                    });
+                  },
+                ),
+                Text('Requires more time on essay assignments.')
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value:isChecked2,
+                  onChanged: (newBool) {
+                    setState(() {
+                      isChecked2 = newBool;
+                    });
+                  },
+                ),
+                Text('Requires more time on assessments and quizes.')
+              ]
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked3,
+                  onChanged: (newBool) {
+                    setState(() {
+                      isChecked3 = newBool;
+                    });
+                  },
+                ),
+                Text('Requires visual aids on some assignments.')
+              ]
+            ),
+            Container(
+              width: 500,
+              padding: EdgeInsets.only(bottom: 5),
+              child: TextField(
+                        decoration: InputDecoration(
+                        labelText: 'Additional Comments (Optional)', // Label inside the text box
+                        border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 0, left: 400),
+              child: ElevatedButton(
+                    onPressed: () {
+                      print('Submitted');
+              },
+                    child: Text('Submit'),
+              )
             ),
           ]
         )
