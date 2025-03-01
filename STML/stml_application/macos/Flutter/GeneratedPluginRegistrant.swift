14972667d7bdfa0ffbacfc353c4791d8c4dbf43a
SECAP-670 <dmani@student.umgc.edu>
2025-02-28 21:27:54 -0500

    Update pubspec.yaml with dependencies, update gradle build versions.

--- STML/stml_application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ STML/stml_application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
+import flutter_tts
+import geolocator_apple
+import local_auth_darwin
+import package_info_plus
+import path_provider_foundation
+import speech_to_text
+import sqflite_darwin
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
