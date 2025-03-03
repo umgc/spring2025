a14b0f2c10c936ae56fa011120f715c9b8c2eb17
Kenneth R. Jones <joneswebconsultingllc@outlook.com>
2025-03-03 12:37:08 -0600

    [Team B] Download Feature

--- team_b/yappy/lib/industry_menu.dart
+++ team_b/yappy/lib/industry_menu.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:yappy/services/database_helper.dart';
 import 'package:share_plus/share_plus.dart';
+// import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart'; // Import path_provider
+import 'dart:io';
+import 'dart:convert'; // Import dart:convert for utf8.encode
+import 'package:yappy/services/file_handler.dart'; // Import file_handler
 
 class IndustryMenu extends StatelessWidget {
   final String title;
   final IconData icon;
 
   const IndustryMenu({required this.title, required this.icon, super.key});
-    Widget generateTranscript(BuildContext context, String title, String content) {
-      return AlertDialog(
-        title: Text(title),
-        content: SingleChildScrollView(
-          child: Text(content),
-        ),
-        actions: [
-          //add export capes
-            Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
+
+  Widget generateTranscript(
+      BuildContext context, String title, String content) {
+    return AlertDialog(
+      title: Text(title),
+      content: SingleChildScrollView(
+        child: Text(content),
+      ),
+      actions: [
+        //add export capes
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
               icon: Icon(Icons.share),
               onPressed: () {
                 // Add your share functionality here
@@ -30,8 +37,28 @@ class IndustryMenu extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.download),
-              onPressed: () {
-                // Add your download functionality here
+              onPressed: () async {
+                try {
+                  final directory = await getApplicationDocumentsDirectory();
+                  final filePath = '${directory.path}/$title.txt';
+                  final file = File(filePath);
+                  await file.writeAsBytes(
+                      utf8.encode(content)); // Convert content to bytes
+
+                  // Use FileHandler to add the document
+                  FileHandler fileHandler = FileHandler();
+                  await fileHandler.addDocument(file);
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Transcript saved to $filePath')),
+                  );
+                  print('File saved at: $filePath');
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to save file: $e')),
+                  );
+                  print('Failed to save file: $e');
+                }
               },
             ),
             IconButton(
@@ -39,22 +66,21 @@ class IndustryMenu extends StatelessWidget {
               onPressed: () {
                 // Add your delete functionality here
               },
-              ),
-            ],
             ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Close'),
-          ),
-        ],
-      );
-    }
+          ],
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Close'),
+        ),
+      ],
+    );
+  }
 
-      Future<List<Map<String, dynamic>>> _fetchTranscripts() async {
+  Future<List<Map<String, dynamic>>> _fetchTranscripts() async {
     DatabaseHelper dbHelper = DatabaseHelper();
-    
     return await dbHelper.getAllTranscripts();
   }
 
@@ -73,21 +99,19 @@ class IndustryMenu extends StatelessWidget {
           Center(
             // Creates the text box above the icons
             child: Container(
-                width: screenWidth * .75,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: const Color.fromARGB(255, 67, 67, 67),
+              width: screenWidth * .75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: const Color.fromARGB(255, 67, 67, 67),
+              ),
+              padding: EdgeInsets.all(12),
+              child: Center(
+                child: Text(
+                  title,
+                  style: TextStyle(fontSize: 24, color: Colors.white),
                 ),
-                padding: EdgeInsets.all(12),
-                child: Center(
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white
-                    ),
-                  ),
-                )),
+              ),
+            ),
           ),
           SizedBox(height: screenHeight * .03),
 
@@ -111,26 +135,26 @@ class IndustryMenu extends StatelessWidget {
                   },
                 ),
               ),
-                SizedBox(width: screenWidth * .06),
+              SizedBox(width: screenWidth * .06),
 
-                // Creates a refine data button
-                //after the transcipt is done it will go to this button to be edited. 
-                //after the edits post it to the database
-                Container(
-                decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+              // Creates a refine data button
+              //after the transcipt is done it will go to this button to be edited.
+              //after the edits post it to the database
+              Container(
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
                 padding: EdgeInsets.all(5),
                 child: IconButton(
                   icon: Icon(
-                  Icons.edit,
-                  color: Colors.white,
-                  size: screenHeight * .05,
+                    Icons.edit,
+                    color: Colors.white,
+                    size: screenHeight * .05,
                   ),
                   onPressed: () {
-                  // Add your refine data functionality here
+                    // Add your refine data functionality here
                   },
                 ),
-                ),
-
+              ),
               SizedBox(width: screenWidth * .06),
 
               // Creates a industry specific icon based on user input
@@ -144,79 +168,76 @@ class IndustryMenu extends StatelessWidget {
                     color: Colors.white,
                     size: screenHeight * .05,
                   ),
-
-                  
                   onPressed: () async {
-                  List<Map<String, dynamic>> transcripts = await _fetchTranscripts();
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                    return Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: const Color.fromARGB(255, 67, 67, 67),
-                      ),
-                      child: Center(
-                      child: Column(
-                        children: [
-                        Expanded(
-                          child: ListView.builder(
-                          itemCount: transcripts.length,
-                          itemBuilder: (context, index) {
-                            Map<String, dynamic> transcript = transcripts[index];
-                            return ListTile(
-                            title: Text(
-                              'Transcript ${transcript['transcript_id']}',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            onTap: () {
-                              Navigator.pop(context);
-                              if (title == 'Restaurant') {
-                              // Show Kanban style list for restaurant
-
-
-
-
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return KanbanBoard(tasks: ['Cheeseburger no lettuce', 'Rootbeer', 'Water with lemon and a large cheese pizza']);
-                                },
-                              );
-                              
-
-
-
-                              } else {
-                              // Show regular transcript for other industries
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                  return generateTranscript(
-                                    context,
-                                    'Transcript',
-                                    transcript['transcript_text_data'] ?? 'No content available',
-                                  );
-                                  },
-                                );
-                              }
-                            }, 
-                            );
-                          },
+                    List<Map<String, dynamic>> transcripts =
+                        await _fetchTranscripts();
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          padding: EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: const Color.fromARGB(255, 67, 67, 67),
                           ),
-                        ),
-                        ],
-                      ),
-                      ),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: ListView.builder(
+                                    itemCount: transcripts.length,
+                                    itemBuilder: (context, index) {
+                                      Map<String, dynamic> transcript =
+                                          transcripts[index];
+                                      return ListTile(
+                                        title: Text(
+                                          'Transcript ${transcript['transcript_id']}',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onTap: () {
+                                          Navigator.pop(context);
+                                          if (title == 'Restaurant') {
+                                            // Show Kanban style list for restaurant
+                                            showModalBottomSheet(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                return KanbanBoard(tasks: [
+                                                  'Cheeseburger no lettuce',
+                                                  'Rootbeer',
+                                                  'Water with lemon and a large cheese pizza'
+                                                ]);
+                                              },
+                                            );
+                                          } else {
+                                            // Show regular transcript for other industries
+                                            showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                return generateTranscript(
+                                                  context,
+                                                  'Transcript',
+                                                  transcript[
+                                                          'transcript_text_data'] ??
+                                                      'No content available',
+                                                );
+                                              },
+                                            );
+                                          }
+                                        },
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     );
-                    },
-                  );                 
-              },
-            ),
-          ),
-          SizedBox(width: screenWidth * .06),
-
+                  },
+                ),
+              ),
+              SizedBox(width: screenWidth * .06),
 
               // Creates a transcript history button
               Container(
@@ -231,17 +252,16 @@ class IndustryMenu extends StatelessWidget {
                   ),
                   onPressed: () async {
                     // Store the context before async operation
-
-                        List<Map<String, dynamic>> transcripts = await _fetchTranscripts();
-
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                          return Container(
+                    List<Map<String, dynamic>> transcripts =
+                        await _fetchTranscripts();
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
                           padding: EdgeInsets.all(16.0),
                           decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: const Color.fromARGB(255, 67, 67, 67),
+                            borderRadius: BorderRadius.circular(12),
+                            color: const Color.fromARGB(255, 67, 67, 67),
                           ),
                           child: Center(
                             child: Column(
@@ -250,13 +270,12 @@ class IndustryMenu extends StatelessWidget {
                                   child: ListView.builder(
                                     itemCount: transcripts.length,
                                     itemBuilder: (context, index) {
-                                      Map<String, dynamic> transcript = transcripts[index];
+                                      Map<String, dynamic> transcript =
+                                          transcripts[index];
                                       return ListTile(
                                         title: Text(
                                           'Transcript ${transcript['transcript_id']}',
-                                          style: TextStyle(
-                                            color: Colors.white
-                                          ),
+                                          style: TextStyle(color: Colors.white),
                                         ),
                                         onTap: () {
                                           Navigator.pop(context);
@@ -264,9 +283,11 @@ class IndustryMenu extends StatelessWidget {
                                             context: context,
                                             builder: (BuildContext context) {
                                               return generateTranscript(
-                                              context,
+                                                context,
                                                 'Transcript',
-                                                transcript['transcript_text_data'] ?? 'No content available',
+                                                transcript[
+                                                        'transcript_text_data'] ??
+                                                    'No content available',
                                               );
                                             },
                                           );
@@ -310,9 +331,8 @@ class KanbanBoardState extends State<KanbanBoard> {
     tasks = widget.tasks;
   }
 
-
-//creates the kanban board sytle widget for the restaurant
-//need to allow the user to edit the order by holding the card
+  //creates the kanban board sytle widget for the restaurant
+  //need to allow the user to edit the order by holding the card
 
   @override
   Widget build(BuildContext context) {
@@ -363,7 +383,8 @@ class KanbanBoardState extends State<KanbanBoard> {
                           IconButton(
                             icon: Icon(Icons.edit),
                             onPressed: () {
-                              TextEditingController controller = TextEditingController(text: tasks[index]);
+                              TextEditingController controller =
+                                  TextEditingController(text: tasks[index]);
                               showDialog(
                                 context: context,
                                 builder: (BuildContext context) {
@@ -371,7 +392,8 @@ class KanbanBoardState extends State<KanbanBoard> {
                                     title: Text('Edit Task'),
                                     content: TextField(
                                       controller: controller,
-                                      decoration: InputDecoration(hintText: 'Enter new task'),
+                                      decoration: InputDecoration(
+                                          hintText: 'Enter new task'),
                                     ),
                                     actions: [
                                       TextButton(
@@ -410,8 +432,7 @@ class KanbanBoardState extends State<KanbanBoard> {
               child: Text('Close'),
             ),
 
-                // Add your save to database functionality here
-
+            // Add your save to database functionality here
           ],
         ),
       ),
