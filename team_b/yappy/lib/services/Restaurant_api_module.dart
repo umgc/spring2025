4dfb9025d33550f11a0f7743fb3cab3c3ff1e120
Racheal Amuneke <rachealsamuel708@gmail.com>
2025-03-05 18:49:28 -0500

    comments taken out

--- team_b/yappy/lib/services/Restaurant_api_module.dart
+++ team_b/yappy/lib/services/Restaurant_api_module.dart
@@ -1,44 +1,3 @@
-// import 'package:speech_to_text/speech_to_text.dart' as stt;
-// import 'database_helper.dart';
-
-// class RestaurantAPI {
-//   final DatabaseHelper dbHelper = DatabaseHelper();
-//   final stt.SpeechToText _speechToText = stt.SpeechToText();
-
-//   Future<void> transcribeAndStoreOrder() async {
-//     bool available = await _speechToText.initialize();
-//     if (available) {
-//       _speechToText.listen(onResult: (result) async {
-//         if (result.finalResult) {
-//           await dbHelper.storeTranscript(result.recognizedWords);
-//         }
-//       });
-//     }
-//   }
-
-//   Future<List<int>> getTranscriptIdsByUserId(int userId) async  {
-//     return await dbHelper.getTranscriptIdsByUserId(userId);
-//   }
-
-//   Future<List<String>> validateMenuItems(String orderText) async {
-//     List<String> menuItems = await dbHelper.getMenuItems();
-//     List<String> validItems = [];
-//     for (var item in menuItems) {
-//       if (orderText.toLowerCase().contains(item.toLowerCase())) {
-//         validItems.add(item);
-//       }
-//     }
-//     return validItems;
-//   }
-
-//   Future<void> correctAndStoreOrder(String correctedOrder) async {
-//     await dbHelper.storeProcessedOrder(correctedOrder);
-//   }
-
-//   Future<List<Map<String, dynamic>>> getOrderHistory() async {
-//     return await dbHelper.getOrderHistory();
-//   }
-// }
 import 'database_helper.dart';
 
 class RestaurantAPI {
@@ -105,7 +64,7 @@ class RestaurantAPI {
   }
 
   // -------------------------------------------------------------------
-  // Optional convenience method that does Steps 5 & 6 in one shot:
+  // Optional convenience method that does Steps 5 & 6 in one shot: 
   // Given a list of extracted items, validate them, then store them.
   // The UI can then call getOrderHistory to show it.
   // -------------------------------------------------------------------
