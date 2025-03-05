bb994af5a52255f3d5afef8d24c184781f354785
Bernhard Zwahlen <bernhard@zwahlen.me>
2025-03-05 11:17:11 -0600

    fix issues from rebase

--- team_b/yappy/lib/speech_state.dart
+++ team_b/yappy/lib/speech_state.dart
@@ -5,7 +5,7 @@ import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:record/record.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:path_provider/path_provider.dart';
 
 import 'package:sherpa_onnx/sherpa_onnx.dart' as sherpa_onnx;
 
