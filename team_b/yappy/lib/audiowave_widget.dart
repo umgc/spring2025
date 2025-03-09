e68afbe5e2634ba9ba141bdf580ff74ef82dce9b
z4sythe <zforsythe@student.umgc.edu>
2025-03-09 15:35:12 -0500

    Flutter suggested fix

--- team_b/yappy/lib/audiowave_widget.dart
+++ team_b/yappy/lib/audiowave_widget.dart
@@ -5,7 +5,7 @@ import 'package:yappy/speech_state.dart';
 class AudiowaveWidget extends StatelessWidget {
   final SpeechState speechState;
 
-  const AudiowaveWidget({Key? key, required this.speechState}) : super(key: key);
+  const AudiowaveWidget({super.key, required this.speechState});
 
   @override
   Widget build(BuildContext context) {
