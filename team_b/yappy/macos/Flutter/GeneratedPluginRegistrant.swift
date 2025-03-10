1fa2c82a2615843cbfd8278c8b4f7afa77ce491c
Racheal Amuneke <rachealsamuel708@gmail.com>
2025-03-09 21:09:42 -0400

    Restaurant API module cleanup

--- team_b/yappy/macos/Flutter/GeneratedPluginRegistrant.swift
+++ team_b/yappy/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,20 @@
 import FlutterMacOS
 import Foundation
 
-import speech_to_text_macos
+import connectivity_plus
+import file_picker
+import path_provider_foundation
+import record_darwin
+import share_plus
+import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
